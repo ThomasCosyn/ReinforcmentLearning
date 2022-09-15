6b4d783a1bd32e8492a8a9679ea3c3cd212ae7b0
thomascosyn <=>
2022-09-15 09:53:46 +0200

    Ajout LAVAF agent

--- agents/discrete_sarsa_agent.py
+++ agents/discrete_sarsa_agent.py
@@ -29,21 +29,22 @@ class TrainingDiscreteSarsaAgent:
     def __init__(self):
         self.action_space = {}
         self.discrete_action_space = np.arange(start = -0.1, stop = 0.11, step = 0.01)
-        self.state = (0, 0, 0, 0, 0)
         self.action_id = len(self.discrete_action_space)//2
         self.months = [i for i in range(1, 13)]
         self.hours = [i for i in range(1, 25)]
-        self.carbon_densities = [0, 0.13, 0.15, 0.18]
+        #self.carbon_densities = [0, 0.13, 0.15, 0.18]
+        self.carbon_densities = [0, 0.126, 0.145, 0.164, 0.186]
         self.costs = [0, 0.3]
         self.battery_storages = [0, 0.25, 0.5, 0.75]
-        self.q_values = np.zeros((len(self.months), len(self.hours), len(self.carbon_densities), len(self.costs), len(self.battery_storages), len(self.discrete_action_space)))
+        self.q_values = np.zeros((len(self.months), len(self.hours), len(self.carbon_densities), len(self.costs), len(self.battery_storages), len(self.costs), len(self.costs), len(self.costs), len(self.discrete_action_space)))
         self.alpha = 0.1
-        self.epsilon = 0.01
-        self.prev_state = (0, 0, 0, 0, 0)
+        self.epsilon = 0.005
+        self.state = (0, 0, 0, 0, 0, 0, 0, 0)
+        self.prev_state = (0, 0, 0, 0, 0, 0, 0, 0)
         self.prev_action_id = len(self.discrete_action_space)//2
         self.algorithm = "SARSA"
         self.choices = [0] * len(self.discrete_action_space)
-        self.discount = 0.9
+        self.discount = 1
 
     def set_action_space(self, agent_id, action_space):
         self.action_space[agent_id] = action_space
@@ -61,7 +62,10 @@ class TrainingDiscreteSarsaAgent:
         carbon_density = [observation[19] < self.carbon_densities[i] for i in range(len(self.carbon_densities))].count(False) - 1
         battery_storage = [observation[22] < self.battery_storages[i] for i in range(len(self.battery_storages))].count(False) - 1
         cost = [observation[24] < self.costs[i] for i in range(len(self.costs))].count(False) - 1
-        self.state = (month - 1, hour - 1, carbon_density, cost, battery_storage)
+        cost6 = [observation[25] < self.costs[i] for i in range(len(self.costs))].count(False) - 1
+        cost12 = [observation[26] < self.costs[i] for i in range(len(self.costs))].count(False) - 1
+        cost24 = [observation[27] < self.costs[i] for i in range(len(self.costs))].count(False) - 1
+        self.state = (month - 1, hour - 1, carbon_density, cost, battery_storage, cost6, cost12, cost24)
 
         # Action choice
         if np.random.random() < self.epsilon:
@@ -85,8 +89,8 @@ class TrainingDiscreteSarsaAgent:
         # Saving RL model
         if observation[0] == 7 and observation[1] == 1 and observation[2] == 23:
             np.save('qvalues.npy', self.q_values)
-            print(self.choices)
-            input()
+            # print(self.choices)
+            # input()
 
         return np.array([action], dtype=self.action_space[agent_id].dtype)
 
