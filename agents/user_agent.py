e8c633de306375ec1fc24a8579ddeb787db8f22d
thomascosyn <=>
2022-09-19 10:20:06 +0200

    Remove scipy to pass the evaluation

--- agents/user_agent.py
+++ agents/user_agent.py
@@ -1,9 +1,9 @@
 from agents.random_agent import RandomAgent
 from agents.rbc_agent import BasicRBCAgent
 from agents.nobattery_agent import NoBatteryAgent
-from agents.intuitiveRBC_agent import IntuitiveRBCAgent, IntuitiveSeasonRBCAgent
-from agents.discrete_sarsa_agent import TrainingDiscreteSarsaAgent
-from agents.lavaf_agent import LavafAgent
+# from agents.intuitiveRBC_agent import IntuitiveRBCAgent, IntuitiveSeasonRBCAgent
+# from agents.discrete_sarsa_agent import TrainingDiscreteSarsaAgent
+# from agents.lavaf_agent import LavafAgent
 
 
 ###################################################################
